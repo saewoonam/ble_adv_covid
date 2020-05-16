ea9ec89340c717565ac1244e0418dc1b7424c53d
Sae Woo Nam <nams@nist.gov>
2020-05-16 12:27:28 -0600

    Add button.c

---
+++ zephyr/bt_cdc_fs/src/button.c
@@ -0,0 +1,63 @@
+#include "led.h"
+#include <stdio.h>
+#include <string.h>
+#include <sys/ring_buffer.h>
+#include <drivers/uart.h>
+#include <fs/fs.h>
+#include <fs/littlefs.h>
+#include <storage/flash_map.h>
+
+extern struct device *cdc_dev;
+extern struct ring_buf outringbuf;
+
+extern struct device *button_dev;
+extern bool write_flash;
+extern int total_written;
+extern int total_cache;
+extern struct ring_buf flashringbuf;
+extern u8_t *flash_buffer;
+
+extern struct fs_file_t encounter_file;
+
+void uart_print(char *buffer, int len);
+
+void button_pressed(struct device *dev, struct gpio_callback *cb,
+            u32_t pins)
+{
+    char buffer[64];
+
+    write_flash = !write_flash;
+    // Flash led crashes things here...???
+    // 
+    //  This doesn't work all the time... Better with irq_lock
+    // int len = sprintf(buffer, "write_flash: %d\n", write_flash);
+    // uart_print(buffer, len);
+}
+
+void button_init(void){
+    int ret;
+    static struct gpio_callback button_cb_data;
+
+    button_dev = device_get_binding(DT_ALIAS_SW0_GPIOS_CONTROLLER);
+    if (button_dev == NULL) {
+        return;
+    }
+
+    ret = gpio_pin_configure(button_dev, DT_ALIAS_SW0_GPIOS_PIN,
+                 DT_ALIAS_SW0_GPIOS_FLAGS | GPIO_INPUT);
+    if (ret != 0) {
+        return;
+    }
+
+    ret = gpio_pin_interrupt_configure(button_dev, DT_ALIAS_SW0_GPIOS_PIN,
+                       GPIO_INT_EDGE_TO_ACTIVE);
+    if (ret != 0) {
+        return;
+    }
+
+    gpio_init_callback(&button_cb_data, button_pressed,
+               BIT(DT_ALIAS_SW0_GPIOS_PIN));
+    gpio_add_callback(button_dev, &button_cb_data);
+
+}
+
